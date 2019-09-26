9208b51f94c3d902192b827465071128bedf0567
Carlos Valcarcel <cvalcarc@asu.edu>
2019-09-26 11:18:37 -0500

    Update clean_data.sh

--- clean_data.sh
+++ clean_data.sh
@@ -11,5 +11,27 @@ echo python/** >> .git/info/sparse-checkout
 git remote add -f origin https://github.com/losDaniel/Student-Voices.git
 git checkout cleaning_script
 git pull origin
-#cd python 
-#python nohup analysis_a2_clean_data.py -c A1 &
+cd python 
+python nohup analysis_a2_clean_data.py -c A1 &
+cd .. 
+cd .. 
+cd clean_B1
+git init
+git config core.sparsecheckout true
+echo python/** >> .git/info/sparse-checkout
+git remote add -f origin https://github.com/losDaniel/Student-Voices.git
+git checkout cleaning_script
+git pull origin
+cd python 
+python nohup analysis_a2_clean_data.py -c B1 &
+cd .. 
+cd .. 
+cd clean_C1
+git init
+git config core.sparsecheckout true
+echo python/** >> .git/info/sparse-checkout
+git remote add -f origin https://github.com/losDaniel/Student-Voices.git
+git checkout cleaning_script
+git pull origin
+cd python 
+python nohup analysis_a2_clean_data.py -c C1 &
