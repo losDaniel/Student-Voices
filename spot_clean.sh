6d0fa6292c0896d60b90df0a0152d4670b629d6d
Carlos Valcarcel <cvalcarc@asu.edu>
2019-09-29 00:02:23 -0500

    Update spot_clean.sh

--- spot_clean.sh
+++ spot_clean.sh
@@ -5,13 +5,13 @@
 # Launch an instance that connects to the reviewdata EFS drive and runs the cleaning scripts:
 
 # Cleaning Instance for A1 Config
-python spot_connect.py -n datac_A1 -p cleaning1 -f reviewdata -s scripts/cleansetup.sh
-python spot_connect.py -n datac_A1 -p cleaning1 -f reviewdata -s scripts/cleandata_A1.sh
+python spot_connect.py -n dtac_A1 -p cleaning1 -f reviewdata -s scripts/cleansetup.sh
+python spot_connect.py -n dtac_A1 -p cleaning1 -f reviewdata -s scripts/cleandata_A1.sh
 
 # Cleaning Instance for B1 Config 
 #python spot_connect.py -n dtac_B1 -p cleaning1 -f reviewdata -s scripts/cleansetup.sh
 #python spot_connect.py -n dtac_B1 -p cleaning1 -f reviewdata -s scripts/cleandata_A1.sh
 
 # Cleaning Instance for C1 Config 
-python spot_connect.py -n datac_C1 -p cleaning1 -f reviewdata -s scripts/cleansetup.sh
-python spot_connect.py -n datac_C1 -p cleaning1 -f reviewdata -s scripts/cleandata_C1.sh
+#python spot_connect.py -n datac_C1 -p cleaning1 -f reviewdata -s scripts/cleansetup.sh
+#python spot_connect.py -n datac_C1 -p cleaning1 -f reviewdata -s scripts/cleandata_C1.sh
