b0b0e1d19f839b81c24b036f8b867020126775fc
Carlos Valcarcel <cvalcarc@asu.edu>
2019-09-28 23:55:32 -0500

    Update cleandata_A1.sh

--- cleandata_A1.sh
+++ cleandata_A1.sh
@@ -17,7 +17,6 @@ git checkout cleaning_script
 git pull origin
 cd scripts 
 python modeling_tools.py
-file = clean_A1.txt
-nohup python analysis_a2_clean_data.py -c A1 &> $file &
+nohup python analysis_a2_clean_data.py -c A1 &> clean_A1.txt &
 curpid=$!
 nohup sh -c 'while ps -p $0 > /dev/null; do sleep 10; done && sudo shutdown -h now' $curpid
