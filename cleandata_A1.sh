f9c4388888e45b784de9fed701b6eafe65ce6b84
Carlos Valcarcel <cvalcarc@asu.edu>
2019-10-01 15:39:09 -0500

    Update SPOT

--- cleandata_A1.sh
+++ cleandata_A1.sh
@@ -4,11 +4,17 @@
 # -- pull scripts directory from github 
 # -- run data cleaning 
 
+# Install Text Blob
 pip install TextBlob
+# Install NLTK
 pip install nltk 
+
+# Create the necessary directories if they're not around
 cd efs
 mkdir scripts
 cd scripts
+
+# Pull the scripts from github
 git init
 git config core.sparsecheckout true
 echo scripts/ >> .git/info/sparse-checkout
@@ -16,7 +22,14 @@ git remote add -f origin https://github.com/losDaniel/Student-Voices.git
 git checkout cleaning_script
 git pull origin
 cd scripts 
+
+# Run the modeling tools script for a preliminary setup 
 python modeling_tools.py
-nohup python analysis_a2_clean_data.py -c A1 &> clean_A1.txt &
+
+# Pull the arguments to pass to the command and save the log 
+ARG=$1
+TXTFILE="clean_$ARG.txt"
+nohup python analysis_a2_clean_data.py -c A1 &> $TXTFILE &
 curpid=$!
+
 nohup sh -c 'while ps -p $0 > /dev/null; do sleep 10; done && sudo shutdown -h now' $curpid
---
+++ spotted.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  1 12:56:39 2019
+
+@author: carlo
+"""
+import spot_toolbox as spt
+import sys
+
+class spotted: 
+    profiles={
+            "default": {'firewall_ingress': ('tcp', 22, 22, '0.0.0.0/0'),      # Must define a firewall ingress rule in order to connect to an instance 
+                        'image_id':'ami-0859ec69e0492368e',                    # Image ID from AWS. go to the launch-wizard to get the image IDs or use the boto3 client.describe_images() with Owners of Filters parameters to reduce wait time and find what you need.
+                        'instance_type':'t2.micro',                            # Get a list of instance types and prices at https://aws.amazon.com/ec2/spot/pricing/ 
+                        'price':'0.004',
+                        'region':'us-west-2',                                  # All settings for us-west-2. Parameters (including prices) can change by region, make sure to review all parameters if changing regions.  
+                        'scripts':[],                            
+                        'username':'ec2-user',                                 # This will usually depend on the operating system of the image used. For a list of operating systems and defaul usernames check https://alestic.com/2014/01/ec2-ssh-username/
+                        'efs_mount':True                                       # If true will check for an EFS mount in the instance and if one is not found it will create a file system or use an existing one and mount it. 
+                        },
+            "datasync":{'firewall_ingress': ('tcp', 22, 22, '0.0.0.0/0'),      # must enable nfs, http and other port ingress depending on endpoints https://docs.aws.amazon.com/datasync/latest/userguide/requirements.html#datasync-network
+                        'image_id':'ami-0f2e06a04ee62ab37',                    # Datasync Image ID from AWS. List by region at https://docs.aws.amazon.com/datasync/latest/userguide/deploy-agents.html#ec2-deploy-agent 
+                        'instance_type':'m5.2xlarge',                          # For recommended instance types for datasync https://docs.aws.amazon.com/datasync/latest/userguide/requirements.html#ec2-instance-types
+                        'price': '0.15',                                       # Spot instance pricing list at https://aws.amazon.com/ec2/spot/pricing/ 
+                        'region':'us-west-2',                                  # All settings for us-west-2. Datasync images vary by region, review all parameters if using a different region
+                        'scripts':[],
+                        'username':'ec2-user',
+                        'efs_mount':False                                      # No need to mount an EFS on a datasync agent (ec2 Instance with a datasync image)
+                        },
+            "cleaning1":{'firewall_ingress': ('tcp', 22, 22, '0.0.0.0/0'),     
+                        'image_id':'ami-0859ec69e0492368e',                    
+                        'instance_type':'c5.4xlarge',                          # VCPU Compute Optimized Instance for data cleaning, bigger chip/more memory -> faster cleaning                     
+                        'price':'0.30',
+                        'region':'us-west-2',                                  
+                        'scripts':[],                            
+                        'username':'ec2-user',                                 
+                        'efs_mount':True                                     
+                        },
+            "ldamodel1":{'firewall_ingress': ('tcp', 22, 22, '0.0.0.0/0'),     
+                        'image_id':'ami-0859ec69e0492368e',                    
+                        'instance_type':'c5d.9xlarge',                          # VCPU Compute Optimized Instance for data cleaning, bigger chip/more memory -> faster cleaning                     
+                        'price':'0.60',
+                        'region':'us-west-2',                                  
+                        'scripts':[],                            
+                        'username':'ec2-user',                                 
+                        'efs_mount':True                                     
+                        },
+#            "gateway":{'image_id':'ami-0a832317c0f4c5d01',}
+    }
+            
+    def __init__(self, name, profile='default', monitoring=True, script='', filesystem='', key_pair='', sec_group=''):
+        
+        self.name=name 
+        self.profile=profile 
+        self.monitoring=monitoring
+        self.filesystem=filesystem
+        self.script=script
+        
+        self.profile=profile 
+        
+        if key_pair!='': 
+            self.profile['key_pair']=profile                                   # if a key pair is specified use it 
+        if sec_group!='':
+            self.profile['security_group']=sec_group                           # if a security group is specified, use it 
+        
+        
+        print('', flush=True)
+        print('#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#', flush=True)
+        print('#~#~#~#~#~#~#~# Launching '+self.name, flush=True)
+        print('#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#', flush=True)
+        print('', flush=True)
+
+        try:                                                   
+            instance, profile = spt.launch_spot_instance(self.name, profile, self.monitoring)  # Launch or connect to the spot instance under the given name 
+        except Exception as e:
+            raise e
+            sys.exit(1)
+    
+        if profile['efs_mount']: 
+            print('Profile requesting EFS mount...')
+            if self.filesystem=='':                                            # If no filesystem name is submitted 
+                fs_name = args.name                                            # Retrieve or create a filesystem with the same name as the instance 
+            else: 
+                fs_name = args.filesystem                                          
+            try:                                                               # Create and/or mount an EFS to the instance 
+                mount_target, instance_dns, filesystem_dns = spt.retrieve_efs_mount(fs_name, instance, new_mount=args.newmount)
+            except Exception as e: 
+                raise e 
+                sys.exit(1)        
+            print('Connecting to instance to link EFS...')
+            spt.run_script(instance, profile['username'], 'efs_mount.sh')
+                
+        st = time.time() 
+    
+        if args.upload!='':        
+            files_to_upload = [] 
+            for file in args.upload.split(','):
+                files_to_upload.append(os.path.abspath(file))
+            spt.upload_to_ec2(instance, profile['username'], files_to_upload, remote_dir=args.remotepath)    
+    
+        print('Time to Upload: %s' % str(time.time()-st))
+    
+        st = time.time() 
+        
+        scripts_to_run = []
+        if args.script!= '': 
+            for s in args.script.split(','):
+                scripts_to_run.append(s)
+    
+        for script in profile['scripts'] + scripts_to_run:
+            print('\nExecuting script "%s"...' % str(script))
+            try:
+                if not spt.run_script(instance, profile['username'], script):
+                    break
+            except Exception as e: 
+                print(str(e))
+                print('Script %s failed with above error' % script)
+    
+        print('Time to Run Scripts: %s' % str(time.time()-st))
+        
+        if args.activeprompt:
+            spt.active_shell(instance, profile['username'])
+    
+        if args.terminate:                                                         # If we want to terminate the instance 
+            spt.terminate_instance(instance['InstanceId'])                             # termination overrrides everything else 
+            print('Script %s has been terminated' % str(args.name))
+            
+            
+            
+            
+            
+    
+        parser = argparse.ArgumentParser(description='Launch spot instance')
+        parser.add_argument('-n', '--name', help='Name of the spot instance', required=True)
+        parser.add_argument('-p', '--profile', help='Profile', default=list(profiles.keys())[0], choices=profiles.keys())
+        parser.add_argument('-s', '--script', help='Script path', default='')
+        parser.add_argument('-f', '--filesystem', help='Elastic File System name', default='')
+        parser.add_argument('-u', '--upload', help='File or directory to upload', default='')
+        parser.add_argument('-r', '--remotepath', help='Directory on EC2 instance to upload via ordinary NFS', default='.')
+        parser.add_argument('-a', '--activeprompt', help='If "True" leave an active shell open after running scripts', default=False)
+        parser.add_argument('-t', '--terminate', help='Terminate the instance after running everything', default=False)
+        parser.add_argument('-nm', '--newmount', help='Create a new mount target even if one exists', default=False)
+        parser.add_argument('-m', '--monitoring', help='Activate monitoring for the instance', default=True)
+        args = parser.parse_args()
