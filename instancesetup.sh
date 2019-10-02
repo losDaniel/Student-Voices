446601eb561a7b453e9a240521ed3eb87fc025aa
Carlos Valcarcel <cvalcarc@asu.edu>
2019-10-02 17:30:32 -0500

    updated sh scripts
    merged in deaggregating results

--- instancesetup.sh
+++ instancesetup.sh
@@ -12,23 +12,18 @@ mkdir data
 mkdir results
 
 cd results 
-
 mkdir LDAdescriptions
 mkdir LDAdistributions
 
 cd .. 
-
 mkdir models
+
 mkdir graphs 
-
 cd graphs 
-
 mkdir LDAGraphs
 
 cd ..
-
 mkdir scripts 
-
 cd scripts
 # Pull the scripts from github
 git init
@@ -38,18 +33,9 @@ git remote add -f origin https://github.com/losDaniel/Student-Voices.git
 git checkout cleaning_script
 git pull origin
 
-
+# Install TextBlob and NLTK
+pip install TextBlob
+pip install nltk 
 
 python -m nltk.downloader all
 sudo python -m nltk.downloader -d /usr/local/share/nltk_data all
-
-
-# Install Text Blob
-pip install TextBlob
-# Install NLTK
-pip install nltk 
-
-# Create the necessary directories if they're not around
-cd efs
-mkdir scripts
-cd scripts 
