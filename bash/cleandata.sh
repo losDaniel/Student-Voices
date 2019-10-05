f46998f6357b355ab4bca039e6c4e73b2d973c29
Carlos Valcarcel <cvalcarc@asu.edu>
2019-10-05 14:24:59 -0500

    dont wait for commands

--- bash/cleandata.sh
+++ bash/cleandata.sh
@@ -18,5 +18,4 @@ nohup python cleaning.py -c DC_CONFIG -o True &> DC_OUTFILE &
 curpid=$!
 
 # Wait until the previous job is done and then shutdown the instance 
-nohup sh -c 'while ps -p $0 > /dev/null; do sleep 10; done && sudo shutdown -h now' $curpid &> run.txt
-exit
+nohup sh -c 'while ps -p $0 > /dev/null; do sleep 10; done && sudo shutdown -h now' $curpid &> run.txt &
